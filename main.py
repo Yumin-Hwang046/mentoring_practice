4f18836d42b3ca4861f831353b6af21a33890b7f
Yumin Hwang <149315511+Yumin-Hwang046@users.noreply.github.com>
2026-03-19 19:53:11 +0900

    [Feature] init setting (#1)
    * uv init
    
    * introduce pre-commit
    
    * update main.py
    
    * fix error

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+
+class Example(object):
+    def __init__(self, bar):
+        if bar:
+            bar += 1
+            bar = bar * bar
+            return bar
+        else:
+            some_string = "foo"
+            return (sys.path, some_string)
+
+
+def main():
+    print("Hello from mentoring-practice!")
+
+
+if __name__ == "__main__":
+    main()
